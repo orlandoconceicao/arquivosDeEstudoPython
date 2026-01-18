52712ce2fd2bcc6b8b9cae02dfe0deab477ba3a8
aapolo7 <orlandoconceicao94@gmail.com>
2026-01-18 18:16:03 -0400

    verificando se conectou

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+# Criando variáveis 
+faturamento = 1500 # Pode substituir o número dado pelo o nome da variavel
+custo = 1000
+lucro = 500
+novasVendas = 333
+faturamento = faturamento + novasVendas # Pode somar variáveis 
+
+# Colocando print dentro do print separado por vírgula
+print("Faturamento:", 1500) # Mais indicado  usar váriaveis
+print("Custo:",custo)
+print("Lucro:", faturamento - custo)
